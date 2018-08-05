a258958d3d0adf5e5605de0a3edd196716d64ca5
gori-san <zer0193yamyam@gmail.com>
2018-08-05 23:12:52 +0900

    add new script

--- Atcoder_script/We_Love_ABC.cpp
+++ Atcoder_script/We_Love_ABC.cpp
@@ -11,26 +11,35 @@ using namespace std;
 int main(){
 	string s;
 	long long int num = 0;
-	int c = 0, k;
+	int c = 0, q = 0, k, l;
 	cin >> s;
 	for(int a = 0; a < s.size(); a++){
-		if(s[a] == 'C' || s[a] == '?') c++;
+		if(s[a] == 'C') c++;
+		if(s[a] == '?') q++;
 	}
 	cout << c << endl;
 	for(int i = 0; i < s.size()-2; i++){
-		if(s[i] == 'C' || s[i] == '?') c--;
+		if(s[i] == 'C') c--;
+		if(s[i] == '?') {
+			q--;
+			l++;
+		}
 		if(s[i] == 'A' || s[i] == '?'){
-			k = 0;
+			k = 0; l = 0;
 			for(int j = i+1; j < s.size()-1; j++){
-				if(s[j] == 'C' || s[j] == '?'){
+				if(s[j] == 'C'){
 					c--;
 					k++;
 				}
+				if(s[j] == '?'){
+					q--;
+					l++;
+				}
 				if(s[j] == 'B' || s[j] == '?'){
-					num += c;
+					num += (c+q)*pow(3,q);
 				}
 			}
-			c += k+1;
+			c += k+1; q += l+1;
 		}
 	}
 	cout << num % (1000000000+7) << endl;
