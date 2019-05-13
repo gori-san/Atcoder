cf7420896af3d1a98b392a829af59e47a4e80f7e
gori-san <zer0193yamyam@gmail.com>
2019-05-13 09:46:43 +0900

    add script

---
+++ diverta_2019_ProgrammingContest/A_ConsecutiveIntegers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#define ll long long int
+#define FOR(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define REP1(i, n) FOR(i, 1, n)
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    cout << n - k + 1 << endl;
+    return 0;
+}
