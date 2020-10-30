859ed29ecc56ba20a8927296f9965f74d5a6eb6e
gori-san <zer0193yamyam@gmail.com>
2020-10-30 19:28:35 +0900

    update ABC179

--- practice/ABC/ABC179/c.py
+++ practice/ABC/ABC179/c.py
@@ -1,2 +1,8 @@
+import math
+
 n = int(input())
 ans = 0
+for i in range(1, n):
+    ans += int(math.floor((n-1)/i))
+
+print(ans)
---
+++ practice/ABC/ABC179/d.py
@@ -0,0 +1,5 @@
+n, k = map(int, input().split())
+l = [0] * k
+r = [0] * k
+for i in range(k):
+    l[i], r[i] = map(int, input().split())
